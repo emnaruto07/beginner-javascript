5ced47ee2a4c2dd82a26c45a5c779f62ea3a8078
emnaruto07 <shazebvps@gmail.com>
2022-02-13 22:29:13 +0530

    comments_x

--- playground/side.js
+++ playground/side.js
@@ -1,12 +1,14 @@
-console.log('hey')
+// console.log('hey')
 
-
+// custom function with return value
 function calculateBill() {
     console.log('Running the function')
     const total = 100 * 1.13;
     return total
 }
 
-const Mytotal = calculateBill()
+//storing the function value in myTotal variable
+const myTotal = calculateBill()
 
-console.log(Mytotal)
+//printing the value
+console.log(myTotal)
