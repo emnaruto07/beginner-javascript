4dd513e676836c2ed60add041f1b5faa581e0d8b
emnaruto07 <shazebvps@gmail.com>
2022-02-13 22:26:55 +0530

    cf_x

---
+++ playground/side.js
@@ -0,0 +1,12 @@
+console.log('hey')
+
+
+function calculateBill() {
+    console.log('Running the function')
+    const total = 100 * 1.13;
+    return total
+}
+
+const Mytotal = calculateBill()
+
+console.log(Mytotal)
