2dac516101262553f3395322a6ebabc38b400e3a
emnaruto07 <shazebvps@gmail.com>
2022-02-16 00:21:06 +0530

    add_classes_wth_js

--- exercises/the-dom.js
+++ exercises/the-dom.js
@@ -1,4 +1,15 @@
 const p = document.querySelector('p')
+const p2 = document.querySelector('h2')
 const divs = document.querySelectorAll('div')
 console.log(p)
-console.log(divs)
+// console.dir(p2)
+
+const test = document.querySelector('.nice')
+console.log(test.classList)
+
+function toggleWhenClick(){
+    test.classList.toggle('round');
+
+}
+
+test.addEventListener('click', toggleWhenClick)
