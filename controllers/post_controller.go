eda4c65733292286f89ce3018d90a2b79f46b8d6
emnaruto07 <shazebvps@gmail.com>
2022-02-17 17:17:53 +0530

    x

---
+++ controllers/post_controller.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/emnaruto07/beginner-javascript/db_client"
+	"github.com/gin-gonic/gin"
+)
+
+type Get struct {
+	Actor_id    int       `json:"id"`
+	First_name  string    `json:"first_name"`
+	Last_name   string    `json:"last_name"`
+	Last_update time.Time `json:"last_update"`
+}
+
+func FetchPost(c *gin.Context) {
+	var posts []Get
+
+	rows, err := db_client.DBClient.Query("select actor_id, first_name,last_name,last_update from actor;")
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"error": true,
+		})
+		return
+	}
+
+	for rows.Next() {
+		var singleRecord Get
+		if err := rows.Scan(&singleRecord.Actor_id, &singleRecord.First_name, &singleRecord.Last_name, &singleRecord.Last_update); err != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"error": true,
+			})
+			return
+		}
+		posts = append(posts, singleRecord)
+	}
+	c.JSON(http.StatusOK, posts)
+}
--- main.go
+++ main.go
@@ -1,59 +1,19 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"time"
-
-	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "gumroaduser"
-	password = "shazeb123"
-	dbname   = "dvdrental"
+	"github.com/emnaruto07/beginner-javascript/controllers"
+	"github.com/emnaruto07/beginner-javascript/db_client"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+	db_client.DBConnection()
 
-	err = db.Ping()
-	if err != nil {
+	r := gin.Default()
+	r.GET("/actors", controllers.FetchPost)
+
+	if err := r.Run(":5000"); err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Successfully connected!")
-
-	var (
-		actor_id    int
-		first_name  string
-		last_name   string
-		last_update time.Time
-	)
-	rows, err := db.Query("select actor_id, first_name,last_name,last_update from actor where actor_id = $1", 3)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		err := rows.Scan(&actor_id, &first_name, &last_name, &last_update)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(actor_id, first_name, last_name, last_update)
-	}
-	err = rows.Err()
-	if err != nil {
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
 }
