a8d7bae1c9ffb2b0bf2c38cbc03fe4d60371b09c
emnaruto07 <shazebvps@gmail.com>
2022-02-18 10:46:24 +0530

    finish_etch

--- exercises/sketch/etch-a-sketch.js
+++ exercises/sketch/etch-a-sketch.js
@@ -1,13 +1,82 @@
 // select the element on the page - canvas, shake button
 
+const canvas = document.querySelector('#etch-a-sketch')
+const ctx =  canvas.getContext('2d')
+const shakebutton = document.querySelector('.shake')
+const MOVE_AMOUNT = 10;
+
 //setup our canvas for drawing
+const width = canvas.width;
+const height = canvas.height;
+
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke()
 
 //write a draw function
+function draw({key}){
+  hue = hue + 5;
+  ctx.strokeStyle = `hsl(${hue},100%, 50%)`
 
-// write a draw function
+  console.log(key)
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+
+  switch (key) {
+    case 'ArrowUp':
+      y = y - MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x = x + MOVE_AMOUNT
+      break;
+    case 'ArrowDown':
+      y = y + MOVE_AMOUNT
+      break;
+    case 'ArrowLeft':
+      x = x - MOVE_AMOUNT
+      break;
+    default:
+      break;
+  }
+
+  // x = x - MOVE_AMOUNT
+  // y = y - MOVE_AMOUNT
+  ctx.lineTo(x, y);
+  ctx.stroke()
+
+
+}
 
 // write a handler for the keys
+function handleKey(e){
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw(({key:e.key}))
+
+  }
+
+}
 
 // clear/shake function
 
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0,0, width,height)
+  canvas.addEventListener('animationend', function(){
+    canvas.classList.remove('shake')
+  }, {once: true})
+  
+}
+
 // listen for arrow keys
+window.addEventListener('keydown', handleKey)
+shakebutton.addEventListener('click', clearCanvas)
