ccba3146163137964578d4f79a1e831674cb8450
emnaruto07 <shazebvps@gmail.com>
2022-02-18 09:46:42 +0530

    starting

---
+++ exercises/sketch/etch-a-sketch.js
@@ -0,0 +1,13 @@
+// select the element on the page - canvas, shake button
+
+//setup our canvas for drawing
+
+//write a draw function
+
+// write a draw function
+
+// write a handler for the keys
+
+// clear/shake function
+
+// listen for arrow keys
